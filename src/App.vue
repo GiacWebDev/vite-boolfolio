613fb081fb85685b6960486e83d89db88ba6a6fc
GiacWebDev <giacobechini@gmail.com>
2023-12-06 16:36:57 +0100

    [init] readme

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      titolo: 'Eccoci su Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ titolo }}</h1>
+</template>
+
+<style lang="scss">
+
+</style>

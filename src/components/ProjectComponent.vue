a7c52f446f7949a1a7a626121e5a7bf44c28f811
GiacWebDev <giacobechini@gmail.com>
2023-12-07 00:37:48 +0100

    [add] ProjectComponent

---
+++ src/components/ProjectComponent.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'ProjectComponent',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  methods: {},
+  mounted() {},
+  computed: {},
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
